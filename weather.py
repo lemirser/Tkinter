a64edb6df3a172f73ac62361102230a79d38b436
Leandro <serzoleandro@gmail.com>
2020-06-01 23:18:04 +0800

    Weather / Air quality (I)

---
+++ weather.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import requests
+import json
+
+root = Tk()
+root.title("Tkinter App")
+root.iconbitmap("icons/gor.ico")
+# root.minsize(300, 250)
+root.geometry("450x40")
+root.configure(background="green")
+
+try:
+    api_request = requests.get(
+        "http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=89129&date=2020-06-01&distance=5&API_KEY=FCD00D59-7954-4D65-813D-837392E665FC"
+    )
+    api = json.loads(api_request.content)
+    city = api[0]["ReportingArea"]
+    quality = api[0]["AQI"]
+    category = api[0]["Category"]["Name"]
+except Exception as e:
+    api = "Error encountered."
+
+myLabel = Label(
+    root, text=city + " Air quality " + str(quality) + " " + category, font=("Helvetica", 20), background="green"
+)
+myLabel.pack()
+
+# Event loop
+root.mainloop()
