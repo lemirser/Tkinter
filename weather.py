09c30658d62e5c244221d8d9027a9d84aaebf54e
Leandro <serzoleandro@gmail.com>
2020-06-01 23:32:54 +0800

    Weather / Air quality (II) -- Background color

--- weather.py
+++ weather.py
@@ -8,7 +8,7 @@ root.title("Tkinter App")
 root.iconbitmap("icons/gor.ico")
 # root.minsize(300, 250)
 root.geometry("450x40")
-root.configure(background="green")
+
 
 try:
     api_request = requests.get(
@@ -18,13 +18,32 @@ try:
     city = api[0]["ReportingArea"]
     quality = api[0]["AQI"]
     category = api[0]["Category"]["Name"]
-except Exception as e:
-    api = "Error encountered."
 
-myLabel = Label(
-    root, text=city + " Air quality " + str(quality) + " " + category, font=("Helvetica", 20), background="green"
-)
-myLabel.pack()
+    if category == "Good":
+        bg_color = "#0C0"
+    elif category == "Moderate":
+        bg_color = "#FFFF00"
+    elif category == "Unhealthy for Sensitive Groups":
+        bg_color = "#ff900"
+    elif category == "Unhealthy":
+        bg_color = "#FF0000"
+    elif category == "Very Unhealthy":
+        bg_color = "#990066"
+    elif category == "Hazardous":
+        bg_color = "#660000"
+
+    myLabel = Label(
+        root,
+        text=city + " Air quality: " + str(quality) + " (" + category + ")",
+        font=("Helvetica", 20),
+        background=bg_color,
+    )
+    root.configure(background=bg_color)
+    myLabel.pack()
+except Exception as e:
+    api = f"Error encountered: {e}"
+    print(api)
+
 
 # Event loop
 root.mainloop()
